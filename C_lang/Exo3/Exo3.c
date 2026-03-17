914d38a74a85d40fd24f1efeb02c8e246a726906
bysocialname <bysocialname@gmail.com>
2026-03-17 16:05:47 +0000

    Apprendre C

---
+++ C_lang/Exo3/Exo3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int s;
+
+    printf("Saisir la valeur de s (le seuil) \n");
+    scanf("%d", &s);
+
+    int Un, n;
+
+    Un = 1;
+    n = 0;
+    
+    while(Un < s){
+
+        Un = (Un*Un) + Un + 1;
+        n++;
+    }
+    printf("La plus petit valeur de n tq Un >= s est : %d", n);
+}
---
+++ C_lang/Exo3/Exo3.md
@@ -0,0 +1,15 @@
+## Exercice 3
+
+On considère une suite définie par
+
+```bash
+U(n+1) = (U(n))^2 + U(n) + 1 et U(o) = 1
+```
+
+On se donne un seuil noté s, calculez la plus petit valeur de n telle que
+
+```bash
+U(n) >= s
+```
+
+Cette exercice peut se voir comme une autre modifications de l'exercice 2.
