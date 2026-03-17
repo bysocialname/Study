547881c28b9d4310c9f633e4c014c75f42f71934
bysocialname <bysocialname@gmail.com>
2026-03-17 15:20:14 +0000

    Apprendre C

---
+++ C_lang/Exo2/Exo2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int a, b, N;
+
+int main(){
+
+    printf("Saisir la valeur de a \n");
+    scanf("%d", &a);
+
+    printf("Saisir la valeur de b \n");
+    scanf("%d", &b);
+
+    printf("Saisir la valeur de N \n");
+    scanf("%d", &N);
+
+    int Un = 1;
+    int sum = Un;
+
+    for(int i = 0; i < N; i++){
+        Un = (a * Un) + b;
+        sum += Un;
+    }
+    printf("U(N) est egale a : %d", Un);
+    printf("\n");
+    printf("La somme des U(n) allant de 0 a N est : %d", sum);
+}
---
+++ C_lang/Exo2/Exo2.md
@@ -0,0 +1,12 @@
+## Exercice 2
+
+On considère une suite définie par
+
+```bash
+U(n+1) = a x U(n) + b et U(o) = 1.
+```
+Connaissant a, b et N, on cherche à calculer
+
+```bash
+U(N) et (Somme des U(n) allant de n = 0 à N)
+```
