bd150ac9056c7982bca7537421f2c1087e28070d
bysocialname <bysocialname@gmail.com>
2026-03-17 00:13:01 +0000

    Apprendre C

---
+++ C_lang/Exo1/Exo1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int a, b, c;
+
+void main(){
+
+    printf("Saisir la valeur de a \n");
+    scanf("%d", &a);
+
+    printf("Saisir la valeur de b \n");
+    scanf("%d", &b);
+
+    printf("Saisirla valeur de c \n");
+    scanf("%d", &c);
+
+    int a_, b_, c_;
+
+    a_ = b;
+    b_ = c;
+    c_ = a;
+    
+    printf("Avant : a = %d, b = %d, c = %d", a, b, c);
+    printf("\n");
+    printf("Apres : a = %d, b = %d, c = %d", a_, b_, c_);
+}
---
+++ C_lang/Exo1/Exo1.md
@@ -0,0 +1,9 @@
+## Exercice 1
+
+On cherche à effectuer une permutation circulaire de trois nombres a, b, c.
+
+```bash
+a' = b
+b' = c
+c' = a
+```
