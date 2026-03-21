27b9fa26da258864172d29e30a8ee7e812153698
bysocialname <bysocialname@gmail.com>
2026-03-21 21:07:47 +0000

    Apprendre C

--- C/Exo1/Exo1.c
+++ C/Exo1/Exo1.c
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-
-int a, b, c;
-
-void main(){
-
-    printf("Saisir la valeur de a \n");
-    scanf("%d", &a);
-
-    printf("Saisir la valeur de b \n");
-    scanf("%d", &b);
-
-    printf("Saisirla valeur de c \n");
-    scanf("%d", &c);
-
-    int a_, b_, c_;
-
-    a_ = b;
-    b_ = c;
-    c_ = a;
-    
-    printf("Avant : a = %d, b = %d, c = %d", a, b, c);
-    printf("\n");
-    printf("Apres : a = %d, b = %d, c = %d", a_, b_, c_);
-    printf("\n");  
-=======
 #include <stdio.h>
 
 int a, b, c;
@@ -50,5 +23,4 @@ void main(){
     printf("\n");
     printf("Apres : a = %d, b = %d, c = %d", a_, b_, c_);
     
->>>>>>> main
 }
