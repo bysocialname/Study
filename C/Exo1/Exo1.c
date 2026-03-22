8a2d4008267ac04ad6beb9a8d6b7cbdb5833745a
bysocialname <bysocialname@gmail.com>
2026-03-22 00:31:36 +0000

    Apprendre C

--- C/Exo1/Exo1.c
+++ C/Exo1/Exo1.c
@@ -2,7 +2,7 @@
 
 int a, b, c;
 
-void main(){
+int main(){
 
     printf("Saisir la valeur de a \n");
     scanf("%d", &a);
@@ -10,7 +10,7 @@ void main(){
     printf("Saisir la valeur de b \n");
     scanf("%d", &b);
 
-    printf("Saisirla valeur de c \n");
+    printf("Saisir la valeur de c \n");
     scanf("%d", &c);
 
     int a_, b_, c_;
@@ -19,8 +19,8 @@ void main(){
     b_ = c;
     c_ = a;
     
-    printf("Avant : a = %d, b = %d, c = %d", a, b, c);
-    printf("\n");
-    printf("Apres : a = %d, b = %d, c = %d", a_, b_, c_);
+    printf("Avant : a = %d, b = %d, c = %d\n", a, b, c);
+    printf("Apres : a = %d, b = %d, c = %d\n", a_, b_, c_);
     
+    return 0;
 }
